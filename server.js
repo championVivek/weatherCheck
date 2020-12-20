476a6df7a12758a240d438d639921dfdccb41a0c
Vivek kumar <vivekkumarse@gmail.com>
2020-12-20 12:59:23 +0530

    error fixed

--- server.js
+++ server.js
@@ -18,22 +18,29 @@ app.get("/", (req, res) => {
 
 app.post("/weather", (req, response) => {
   let city = req.body.city;
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.API_KEY}`;
-
-  axios.get(url).then((res) => {
-    let weather = res.data
-    if (weather.main === undefined) {
-      response.render("index", {
-        weather: null,
-        error: "Error, please try again!",
-      });
-    } else {
-      let weat = weather.main.temp;
-      let celsius = parseFloat(((weat - 32) * 5) / 9).toFixed(2);
-      let weatherText = `It's ${celsius}°C in ${weather.name}`;
-      response.render("index", { weather: weatherText, error: null });
-    }
-  });
+  if (city === "") {
+    console.log("Enter city");
+    response.render("index", {
+      weather: null,
+      error: "Please enter city!",
+    });
+  } else {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.API_KEY}`;
+    axios.get(url).then((res) => {
+      let weather = res.data;
+      if (weather.main === undefined) {
+        response.render("index", {
+          weather: null,
+          error: "Error, please try again!",
+        });
+      } else {
+        let weat = weather.main.temp;
+        let celsius = parseFloat(((weat - 32) * 5) / 9).toFixed(2);
+        let weatherText = `It's ${celsius}°C in ${weather.name}`;
+        response.render("index", { weather: weatherText, error: null });
+      }
+    });
+  }
 });
 
 let PORT = process.env.PORT || 3000;
