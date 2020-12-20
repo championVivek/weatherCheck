42d8d001831af60d9a0e00ae544f55efaa9d6e97
Vivek kumar <vivekkumarse@gmail.com>
2020-12-20 12:40:44 +0530

    axios added

--- server.js
+++ server.js
@@ -1,39 +1,37 @@
 const express = require("express");
 const request = require("request");
-const cors = require('cors')
+const axios = require("axios");
+const cors = require("cors");
 require("dotenv").config();
 const bodyParser = require("body-parser");
-
 
 const app = new express();
 
 app.set("view engine", "ejs");
 
-app.use(cors())
+app.use(cors());
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.render("index", { weather: null, error: null });
 });
-app.post("/", (req, res) => {
+
+app.post("/", (req, response) => {
   let city = req.body.city;
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.API_KEY}`;
-  request(url, (err, response, body) => {
-    if (err) {
-      res.render("index", { weather: null, error: "Error, please try again" });
+
+  axios.get(url).then((res) => {
+    let weather = res.data
+    if (weather.main === undefined) {
+      response.render("index", {
+        weather: null,
+        error: "Error, please try again!",
+      });
     } else {
-      let weather = JSON.parse(body);
-      if (weather.main === undefined) {
-        res.render("index", {
-          weather: null,
-          error: "Error, please try again!",
-        });
-      } else {
-        let weat = weather.main.temp;
-        let celsius = parseFloat(((weat - 32) * 5) / 9).toFixed(2);
-        let weatherText = `It's ${celsius}°C in ${weather.name}`;
-        res.render("index", { weather: weatherText, error: null });
-      }
+      let weat = weather.main.temp;
+      let celsius = parseFloat(((weat - 32) * 5) / 9).toFixed(2);
+      let weatherText = `It's ${celsius}°C in ${weather.name}`;
+      response.render("index", { weather: weatherText, error: null });
     }
   });
 });
