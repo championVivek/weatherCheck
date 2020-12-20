514820e67be6528291d622296361049dfda9dbdb
Vivek kumar <vivekkumarse@gmail.com>
2020-12-20 12:14:22 +0530

    api changed

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 });
 app.post("/", (req, res) => {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.API_KEY}`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.API_KEY}`;
   request(url, (err, response, body) => {
     if (err) {
       res.render("index", { weather: null, error: "Error, please try again" });
